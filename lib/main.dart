1038f25c673e472cd839ee8d5f2e9e4b16ab1cfa
Ammar Shafiq <Ammardev99@gmail.com>
2025-09-23 15:28:47 +0500

    counter

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Counter'),
+            const Text('Counter Value:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
